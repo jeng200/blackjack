646a9e547e0e289cc5fb3932ce56468676977041
jeng200 <jeng200@yahoo.com>
2021-11-14 17:05:15 -0500

    Add NEWCARD button, function, and adjust CSS

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-let firstCard = 11
+let firstCard = 4
 let secondCard = 10
 let sum = firstCard + secondCard
 let hasBlackJack = false
 let isAlive = true
 let message = ""
 let messageEl = document.getElementById("message-el")
+let sumEl = document.getElementById("sum-el")
+let cardsEl = document.getElementById("cards-el")
 
 function startGame(){
+  cardsEl.textContent = "Cards: " + firstCard + " " + secondCard
+  sumEl.textContent = "Sum: " + sum
   if (sum < 21) {
     message = "Do you want to draw a new card?"
   } else if (sum === 21) {
@@ -17,4 +21,11 @@ function startGame(){
     let isAlive = false
   }
   messageEl.textContent = message
+}
+
+function newCard() {
+  console.log("Drawing a new card from the deck!")
+  let card = 6
+  sum += card
+  startGame()
 }
